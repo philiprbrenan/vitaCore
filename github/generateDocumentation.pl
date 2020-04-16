4c0740e9fe79e431ffa68672213c539d97eecc63
philip r brenan <philiprbrenan@gmail.com>
2020-04-16 11:55:25 +0100

    Vita_on_2020_04_16_at_11_55_25

--- github/generateDocumentation.pl
+++ github/generateDocumentation.pl
@@ -9,15 +9,24 @@ use Carp;
 use Data::Dump qw(dump);
 use Data::Table::Text qw(:all);
 
-my $home    = q(/home/phil/vita/minimum/);                                      # Home folder
-my $modules = fpd($home, q(modules));                                           # Modules folder
-my $tests   = fpd($home, q(tests));                                             # Test programs
+my $home    = q(/home/phil/vita/vitaCore/);                                     # Home folder
+my $modules = fpd($home, qw(vita modules));                                     # Modules folder
 my $docs    = fpd($home, q(docs));                                              # Output documentation
+
+my @errors;
 
 makePath($docs);
 clearFolder($docs, 99);
 
-my @files = searchDirectoryTreesForMatchingFiles($modules, $tests, qw(.py));    # Modules to document
-my $files = join ' ', @files;
+my @files = searchDirectoryTreesForMatchingFiles($modules, qw(.py));            # Modules to document
 
-say STDERR qx(/home/phil/.local/bin/pycco -d $docs -ie $files);
+#say STDERR qx(/home/phil/.local/bin/pycco -d $docs -ie $files);
+
+for my $source(@files)
+ {say STDERR $source;
+  my $target = fpe($docs, fn($source), q(html));
+  my $r = extractPythonDocumentation($source, $target);
+  push @errors, $r->errors->@*;
+ }
+
+owf(fpe($docs, qw(aaaErrors txt)), join "\n", @errors);
